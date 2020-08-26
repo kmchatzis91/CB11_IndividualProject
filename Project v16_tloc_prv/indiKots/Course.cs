b7db758466d2027e8796d194dac69a14b982449f
Konstantinos Chatzis <67655091+kmchatzis91@users.noreply.github.com>
2020-08-26 14:47:20 +0300

    Add files via upload

---
+++ Project v16_tloc_prv/indiKots/Course.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indiKots
+{
+	class Course : Helper
+	{
+		public string Title { get; private set; }
+		public string Stream { get; private set; }
+		public string Type { get; private set; }
+		public DateTime StartDate { get; private set; }
+		public DateTime EndDate { get; private set; }
+		public int CourseID { get; private set; }
+		private static int TotalCourses = 0;
+
+		public Course(string title, string stream, string type, DateTime startdt, DateTime enddt)
+		{
+			Title = title;
+			Stream = stream;
+			Type = type;
+			StartDate = startdt;
+			EndDate = enddt;
+			TotalCourses++;
+			CourseID = TotalCourses;
+
+		} //--- 1st constructor Course end ---//
+
+		public Course()
+		{
+			TotalCourses++;
+			CourseID = TotalCourses;
+
+		} //--- 2nd constructor Course end ---//
+
+		public void SetCourse()
+		{
+			Console.WriteLine(" Ok let's make the list of Courses! ");
+			Console.WriteLine("\n The information about each Course you need to give is: Title, Stream, Type, Start Date and End Date. ");
+			Console.WriteLine(" Pay attention to the following example: ");
+			Console.WriteLine("\n\n Coding BootCamp 11, Java, Full-Time, 2020,07,15, 2020,09,15 \n");
+			Console.WriteLine("\n Give course's title (ex. Coding BootCamp 11): ");
+			Title = Console.ReadLine();
+			Console.WriteLine(" Give course's stream (ex. Java): ");
+			Stream = Console.ReadLine();
+			Console.WriteLine(" Give course's type (ex. Full-Time): ");
+			Type = Console.ReadLine();
+			Console.WriteLine(" Give course's start date (ex. 2020,07,15): ");
+			StartDate = ValidateDateTime();
+			Console.WriteLine(" Give course's end date (ex. 2020,09,15): ");
+			EndDate = ValidateDateTime();
+
+		} //--- SetCourse method end ---//
+
+	} //--- class Course end ---//
+
+} //--- namespace end ---//
---
+++ Project v16_tloc_prv/indiKots/Helper.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indiKots
+{
+	class Helper
+	{
+		public Helper() 
+		{
+
+		} //--- 1st constructor Helper end ---//
+
+		protected DateTime ValidateDateTime()
+		{
+			DateTime ValidDateTime = new DateTime();
+			bool IsValid = false;
+			while (!IsValid)
+			{
+				IsValid = DateTime.TryParse(Console.ReadLine(), out ValidDateTime);
+				if (!IsValid)
+				{
+					dtMess();
+				}
+			}
+
+			return ValidDateTime;
+
+		} //--- protected DateTime ValidateDateTime() end ---//
+
+		protected int ValidateMark()
+		{
+			int ValidInt = 0;
+			bool IsValid = false;
+			while (!IsValid)
+			{
+				IsValid = Int32.TryParse(Console.ReadLine(), out ValidInt);
+				if (!IsValid)
+				{
+					intMess();
+
+					if (IsValid && ValidInt < 0)
+					{
+						ValidInt = 0;
+					}
+					else if (IsValid && ValidInt > 100)
+					{
+						ValidInt = 100;
+					}
+				}
+				else
+				{
+					if (ValidInt < 0)
+					{
+						ValidInt = 0;
+					}
+					else if (ValidInt > 100)
+					{
+						ValidInt = 100;
+					}
+				}
+			}
+
+			return ValidInt;
+
+		} //--- protected int ValidateMark() end ---//
+
+		protected int ValidateTuitionFees()
+		{
+			int ValidInt = 0;
+			bool IsValid = false;
+			while (!IsValid)
+			{
+				IsValid = Int32.TryParse(Console.ReadLine(), out ValidInt);
+				if (!IsValid)
+				{
+					intMess();
+
+					if (IsValid && ValidInt < 0)
+					{
+						ValidInt = 0;
+					}
+					else if (IsValid && ValidInt > 50000)
+					{
+						ValidInt = 50000;
+					}
+				}
+				else
+				{
+					if (ValidInt < 0)
+					{
+						ValidInt = 0;
+					}
+					else if (ValidInt > 50000)
+					{
+						ValidInt = 50000;
+					}
+				}
+			}
+
+			return ValidInt;
+
+		} //--- protected int ValidateTuitionFees() end ---//
+
+		protected int ValidateGivenID()
+		{
+			int ValidInt = 0;
+			bool IsValid = false;
+			while (!IsValid)
+			{
+				IsValid = Int32.TryParse(Console.ReadLine(), out ValidInt);
+				if (!IsValid)
+				{
+					intMess();
+				}
+			}
+
+			return ValidInt;
+
+		} //---protected int ValidateGivenID() end ---//
+
+		protected void dtMess() 
+		{
+			Console.WriteLine(" The Date you gave was not valid!!! ");
+			Console.WriteLine(" The correct format is: YYYY,MM,DD ");
+
+		} //--- protected void dtMess() end ---//
+
+		protected void intMess()
+		{
+			Console.WriteLine(" The number you gave was not valid!!! ");
+			Console.WriteLine(" Please give a valid value (integer) ");
+
+		} //--- protected void intMess() end ---//
+
+	} //--- class Helper end ---//
+
+} //--- namespace end ---//
